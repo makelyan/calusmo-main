6fd25e0fe6c17f39ee7581b9c72c2bda1073afc4
emi-sakamoto <ejsakamoto3@gmail.com>
2024-03-18 18:41:16 -0700

    services additions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,13 +12,33 @@ export default function() {
                     <p className = "sub1">Under Graduate Street Medicine Outreach</p>
                     <p className= "sub2" >subtitle about what USMO is catchy</p>
                 </div>
-                <div className = "mission">
-                    <h2>Our Mission</h2>
-                    <p>Our mission is to overcome and mitigate barriers that houseless communities/encampments face in relation to accessing healthcare and other services provided in the Berkeley area.</p>
-                </div>
             </div>
+            <div className = "mission">
+                <h2>Our Mission</h2>
+                 <p className = "missionp">Our mission is to overcome and mitigate barriers that houseless communities/encampments face in relation to accessing healthcare and other services provided in the Berkeley area.</p>
+            </div>
+
             <div className = "services">
-                <h1>Our Services</h1>
+            <h2>Our Services</h2>
+                <div className = "wrapper0">
+
+                    <div className = "icon-container">
+                        <img src="physical_exams.png" alt="Icon 1"></img>
+                        <p>Physical Exams</p>
+                    </div>
+
+                    <div className = "icon-container">
+                        <img src="mental_health.png" alt="Icon 2"></img>
+                        <p>Mental Health</p>
+                    </div>
+
+                    <div className = "icon-container">
+                        <img src="flu_shot.png" alt="Icon 3"></img>
+                        <p>Flu Shot</p>
+                    </div>
+
+                </div>
+               
             </div>
 
 
