5258b793f7061d4cefd7c7a5de748a9cbf3ede67
MeatJun <meatjun@berkeley.edu>
2024-03-18 18:37:41 -0700

    Finished rough draft of navbar and contact us page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import "../styles/Home.css"
+import "../styles/index.css"
 
 export default function() {
     return (
@@ -10,23 +10,15 @@ export default function() {
                 <div className="hero-contents">
                     <h1 className = "title">USMO</h1>
                     <p className = "sub1">Under Graduate Street Medicine Outreach</p>
-                    <p className= "sub2" > Berkeley's Student organization dedicated to supporting the homeless community of Berkeley</p>
+                    <p className= "sub2" >subtitle about what USMO is catchy</p>
                 </div>
                 <div className = "mission">
                     <h2>Our Mission</h2>
-                    <p className = "missionp">Our mission is to overcome and mitigate barriers that houseless communities/encampments face in relation to accessing healthcare and other services provided in the Berkeley area.</p>
+                    <p>Our mission is to overcome and mitigate barriers that houseless communities/encampments face in relation to accessing healthcare and other services provided in the Berkeley area.</p>
                 </div>
             </div>
             <div className = "services">
-            <h2>Our Services</h2>
-                <div className = "wrapper">
-                    <ul className="ul_div">
-                        <li>Physical exams</li>
-                        <li> Mental Health</li>
-                        <li> Flu Shot</li>
-                    </ul>
-                </div>
-               
+                <h1>Our Services</h1>
             </div>
 
 
