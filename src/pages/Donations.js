5726e7f13782e5f4543934ba7d3625778d2a1cd9
Juyeon <jyma0218@berkeley.edu>
2024-04-09 18:12:41 -0700

    modified donations page

--- src/pages/Donations.js
+++ src/pages/Donations.js
@@ -1,21 +1,22 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import React from 'react'
+import "../styles/Donations.css"
 
 export default function() {
+    const handleDonateClick = () => {
+        // Redirect the user to the GoFundMe page
+        window.location.href = 'https://gofund.me/81a81395';
+    };
+
     return (
         <>
             <Header />
             <h2>Donation Page</h2>
-            <form action="https://www.paypal.com/donate" method="post" target="_top">
-<input type="hidden" name="business" value="27TQS5YAEL9SG" />
-<input type="hidden" name="no_recurring" value="0" />
-<input type="hidden" name="currency_code" value="USD" />
-<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-<img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-</form>
-
-
+            <p>USMO is a nonprofit student-based organization, so 100% of the services and resources that we employ to help our community are funded and facilitated by each of our members personally. As a result, any donation that can be spared would be a tremendous help in helping us accomplish our mission of providing accessible healthcare to our community. Please fill out the form below if you're interested in donating to USMO or contact us at our e-mail or phone number on our website's "contact" section if there's something else you'd like to offer.</p>
+            {/* Use a button with an onClick event listener */}
+            <button onClick={handleDonateClick}>Donate Now</button>
             <Footer />
         </>
-    )
+    );
 }
