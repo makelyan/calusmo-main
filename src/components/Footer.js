325807778af6d3f7a6d5af0bcd1e60c8f3611cd8
MeatJun <meatjun@berkeley.edu>
2025-01-09 17:30:24 -0800

    Deployment fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,12 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDiscord, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
     <footer className="bg-light text-center text-lg-start">
         <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          <a href="https://discord.gg/XCngeXWusK" rel="noopener noreferrer" target="_blank" className="text-dark me-4">
-            <FontAwesomeIcon icon={faDiscord} />
-          </a>
           <a href="https://www.linkedin.com/company/usmoberkeley/" rel="noopener noreferrer" target="_blank" className="text-dark me-4">
             <FontAwesomeIcon icon={faLinkedin} />
           </a>
